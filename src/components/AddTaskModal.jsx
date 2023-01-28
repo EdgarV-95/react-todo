b6faa67ed72befc8bb1a5c0fc48dbc2b8d298e78
EdgarV-95 <edgar.veress@yahoo.com>
2023-01-28 16:30:59 +0000

    feat; Added all fields to AddTaskModal and finished its styling

--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -1,20 +1,84 @@
 import React from 'react';
+import './AddTaskModal.css';
+import {
+  TextField,
+  Stack,
+  Box,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@mui/material';
 
 export default function AddTaskModal({ closeModal }) {
   return (
     <div className="modal-background">
       <div className="modal-body">
-        <h3>New Task</h3>
-        <div className="form-body">
+        <div className="modal-header">
+          <h3>New Task</h3>
           <button onClick={closeModal}>X</button>
+        </div>
+        <div className="form-body">
           <ul>
-            <li>Title</li>
-            <li>Description</li>
+            <TextField
+              id="outlined-basic"
+              label="Title"
+              variant="outlined"
+            />
+            <TextField
+              id="outlined-multiline-static"
+              label="Description"
+              multiline
+              rows={4}
+            />
           </ul>
           <ul>
-            <li>Due Date</li>
-            <li>Priority</li>
-            <li>Project</li>
+            <Stack component="form" noValidate>
+              <TextField
+                id="date"
+                label="Date"
+                type="date"
+                defaultValue="2023-01-28"
+                sx={{ width: 220 }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Stack>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Priority
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  // value={value}
+                  label="Priority"
+                  onChange={() => console.log('lol')}
+                >
+                  <MenuItem value={'Low'}>Low</MenuItem>
+                  <MenuItem value={'Medium'}>Medium</MenuItem>
+                  <MenuItem value={'High'}>High</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Project
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  // value={value}
+                  label="Project"
+                  onChange={() => console.log('lol')}
+                >
+                  <MenuItem value={'Inbox'}>Inbox</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           </ul>
         </div>
         <div className="buttons">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import AddIcon from '@mui/icons-material/Add';
 import DoneOutlinedIcon from '@mui/icons-material/DoneOutlined';
-// import AddTaskModal from './AddTaskModal';
+import AddTaskModal from './AddTaskModal';
 
 export default function Header({ showSidebar, onSidebarToggle }) {
   const [addTaskModal, setAddTaskModal] = useState(false);
@@ -12,7 +12,11 @@ export default function Header({ showSidebar, onSidebarToggle }) {
   return (
     <div className="top-navbar">
       <div className="nav-left">
-        <div onClick={() => onSidebarToggle(showSidebar)}>
+        <div
+          onClick={() => {
+            onSidebarToggle(showSidebar);
+          }}
+        >
           <MenuIcon sx={{ fontSize: 30 }} />
         </div>
         <div onClick={() => console.log('Takes you back home')}>
@@ -26,36 +30,8 @@ export default function Header({ showSidebar, onSidebarToggle }) {
         {addTaskModal && (
           <AddTaskModal closeModal={() => setAddTaskModal(false)} />
         )}
-
         <div onClick={() => console.log('Shows list of done tasks')}>
           <DoneOutlinedIcon sx={{ fontSize: 30 }} />
-        </div>
-        <button onClick={() => console.log(addTaskModal)}></button>
-      </div>
-    </div>
-  );
-}
-
-function AddTaskModal({ closeModal }) {
-  return (
-    <div className="modal-background">
-      <div className="modal-body">
-        <h3>New Task</h3>
-        <div className="form-body">
-          <button onClick={closeModal}>X</button>
-          <ul>
-            <li>Title</li>
-            <li>Description</li>
-          </ul>
-          <ul>
-            <li>Due Date</li>
-            <li>Priority</li>
-            <li>Project</li>
-          </ul>
-        </div>
-        <div className="buttons">
-          <button onClick={closeModal}>Close</button>
-          <button onClick={() => console.log('works')}>Submit</button>
         </div>
       </div>
     </div>
