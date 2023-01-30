45f56eb4923328e644259b4a2a3c704ed1e51e9b
EdgarV-95 <edgar.veress@yahoo.com>
2023-01-30 19:38:26 +0000

    more trial and error with the priority picker

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,14 +7,13 @@ import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined
 import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 
 function PriorityPicker() {
-  console.log('here');
   return (
     <>
-      <ul className="bazdmeg">
+      <div className="bazdmeg">
         <li>Low</li>
         <li>Medium</li>
         <li>High</li>
-      </ul>
+      </div>
     </>
   );
 }
@@ -31,9 +30,14 @@ export default function Body() {
   }
 
   const [click, setClick] = useState(false);
-  function handlePriority(value) {
+  function handlePriority(e, idValue) {
     // Create component to select new priority and save it to body and localstorage
-    if (value) setClick(!click);
+    let newTask = tasks.filter((t) => t.id === idValue);
+    // Use data attribute to get the item
+    // Find a way to have the div over the icon. Maybe set its position to absolute?
+    console.log(e);
+    console.log(newTask[0].id);
+    if (idValue === newTask[0].id) setClick(!click);
   }
 
   return (
@@ -43,28 +47,34 @@ export default function Body() {
         <ul className="tasks-list">
           {tasks.map((task) => {
             return (
-              <li key={task.id} className="task">
-                <div className="task-left">
-                  <CheckCircleOutlineOutlinedIcon className="task-done" />
-                  <div className="task-title">{task.titleValue}</div>
-                </div>
-                <div className="task-right">
-                  <EditOutlinedIcon className="task-edit" />
-                  <FlagIcon
-                    className="task-priority"
-                    onClick={() => {
-                      console.log(click);
-                      handlePriority(task.priorityValue);
-                    }}
-                  />
-                  {click && <PriorityPicker />}
-                  <ArrowCircleRightOutlinedIcon className="task-project" />
-                  <DeleteOutlineOutlinedIcon
-                    className="task-delete"
-                    onClick={() => handleDelete(task.id)}
-                  />
-                </div>
-              </li>
+              <>
+                <li key={task.id} className="task">
+                  <div className="task-left">
+                    <CheckCircleOutlineOutlinedIcon className="task-done" />
+                    <div className="task-title">
+                      {task.titleValue}
+                    </div>
+                  </div>
+                  <div className="task-right">
+                    <EditOutlinedIcon className="task-edit" />
+                    {click && <PriorityPicker />}
+                    <div className="task-test" data-info="test">
+                      <FlagIcon
+                        className="task-priority"
+                        onClick={(e) => {
+                          handlePriority(e, task.id);
+                        }}
+                      />
+                    </div>
+
+                    <ArrowCircleRightOutlinedIcon className="task-project" />
+                    <DeleteOutlineOutlinedIcon
+                      className="task-delete"
+                      onClick={() => handleDelete(task.id)}
+                    />
+                  </div>
+                </li>
+              </>
             );
           })}
         </ul>
