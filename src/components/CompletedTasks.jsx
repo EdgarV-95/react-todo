09c3d7cec9e842be241bc34c5c72158038d3149b
Edgar Veress <veress.edgar@yahoo.com>
2023-02-16 22:34:21 +0000

    quick update

--- src/components/CompletedTasks.jsx
+++ src/components/CompletedTasks.jsx
@@ -1,26 +1,10 @@
 import { useState } from 'react';
 import TaskDesc from './options/TaskDesc';
-import DeleteTask from './options/DeleteTask';
 
 export default function CompletedTasks() {
   const [completedList, setCompletedList] = useState(
     JSON.parse(localStorage.getItem('done') || '[]')
   );
-
-  // Handle DETAILS
-  const [taskIndex, setTaskIndex] = useState(null);
-  function toggleTask(index) {
-    setTaskIndex(index === taskIndex ? null : index);
-  }
-
-  // Handle DELETE
-  function handleDelete(idValue) {
-    let remainingTasks = completedList.filter(
-      (task) => task[0].id !== idValue
-    );
-    localStorage.setItem('done', JSON.stringify(remainingTasks));
-    setCompletedList(remainingTasks);
-  }
 
   return (
     <div className="completed-tasks">
